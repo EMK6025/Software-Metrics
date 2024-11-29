e7570595379f62374366652e7939bb48525c8e2e
EMK6025 <emk6025@psu.edu>
2024-11-28 20:24:12 -0500

    made process_file.py function as a subprocess

--- test_suite/process_file.py
+++ test_suite/process_file.py
@@ -2,38 +2,37 @@ import os
 import subprocess
 import sys
 
-# Ensure enough arguments are passed
-if len(sys.argv) != 4:
-    print("Usage: python process_file.py <dir_path> <file_name> <metric_name>")
-    sys.exit(1)
+def process_file(project_dir, file_name, metric_name):
+    dir_path = os.path.join('../projects/', project_dir)
+    file_path = os.path.join(dir_path, file_name)
+    metric_path = os.path.join('../metrics/', metric_name)
 
-# Get the values from the command line arguments
-dir = sys.argv[1]
-file_name = sys.argv[2]
-metric_name = sys.argv[3]
+    # Ensure the metric file exists
+    if os.path.exists(metric_path):
+        # Run metric
+        result = subprocess.run(['python', metric_path, file_path], capture_output=True, text=True)
 
-# Relative file path to the database
-db_path = '../database.db'
+        if result.returncode == 0:
+            try:
+                # Write the output into output.txt
+                metric_val = result.stdout.strip()
+                output_file_path = os.path.join(dir_path, "output.txt")
+                with open(output_file_path, 'w') as output_file:
+                    output_file.write(f"{metric_name} {file_name} {metric_val}")
+            except ValueError:
+                sys.exit()
+            except IOError:
+                sys.exit()
 
-dir_path = os.path.join('../projects/', dir)
-file_path = os.path.join(dir_path, file_name)
-metric_path = os.path.join('../metrics/', metric_name)
 
-# Ensure the metric file exists
-if os.path.exists(metric_path):
-    # Run metric
-    result = subprocess.run(['python', metric_path, file_path], capture_output=True, text=True)
+if __name__ == "__main__":
+    # Ensure enough arguments are passed
+    if len(sys.argv) != 4:
+        print("Usage: python process_file.py <dir_path> <file_name> <metric_name>")
+        sys.exit(1)
 
-    if result.returncode == 0:
-        try:
-            # Write the output into output.txt
-            metric_val = result.stdout.strip()
-            output_file_path = os.path.join(dir_path, "output.txt")
-            with open(output_file_path, 'w') as output_file:
-                output_file.write(metric_name + " " + file_name + " " + metric_val)
-        except ValueError:
-            print("ValueError: script has output formatting issues.")
-    else:
-        print(f"Runtime error in script: {result.stderr}")
-else:
-    print(f"Metric script not found at {metric_path}")
+    # Get the values from the command line arguments
+    project_dir = sys.argv[1]
+    file_name = sys.argv[2]
+    metric_name = sys.argv[3]
+    process_file(project_dir, file_name, metric_name)
