940803f3b9feac183e64d361671a2694e2f7711a
EMK6025 <emk6025@psu.edu>
2024-11-28 20:10:59 -0500

    made boiler_plate.py function accurately to be useable as a subprocess

--- metrics/boiler_plate.py
+++ metrics/boiler_plate.py
@@ -1,25 +1,31 @@
 import sys
 
 def metric(file_path):
+    count = 0
     # Open the file in read mode
     try:
         with open(file_path, 'r', encoding='utf-8') as java_file:
             for line in java_file:
                 count += 1
                 #do something
+        return count
+    # Handle exceptions
     except FileNotFoundError:
         print(f"File not found: {file_path}")
+        return "Error"
     except IOError as e:
         print(f"An error occurred: {e}")
+        return "Error"
 
 
 if __name__ == "__main__":
+    # Ensure enough arguments are passed
     if len(sys.argv) != 2:
-        print("Usage: python \{metrics_name\}.py <dir> <metric_name>")
+        print("Usage: python <metrics_name>.py <file_path>")
         sys.exit(1)
 
     # Get the values from the command line arguments
     file_path = sys.argv[1]
 
-    result = metric(file_path)
-    print(result)
+    # Print the result of metric
+    print(metric(file_path)) 
