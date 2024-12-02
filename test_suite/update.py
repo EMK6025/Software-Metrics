61b73bbd32847133e64687f4b84abdd48e206e4f
EMK6025 <emk6025@psu.edu>
2024-12-02 15:48:47 -0500

    change update.py to only add folders and not subfolders

--- test_suite/update.py
+++ test_suite/update.py
@@ -11,24 +11,24 @@ cursor = conn.cursor()
 insert_cmd = "INSERT INTO projects (project_name, path_to_folder) VALUES (?, ?)"
 check_cmd = "SELECT 1 FROM projects WHERE project_name = ? LIMIT 1"
 
-# Walk through projects/ and add new dirs in
-for root, dirs, _ in os.walk('../projects/'):
-    for dir in dirs:
-        # Get the full file path
-        folder_path = os.path.join(root, dir)
-
+for item in os.listdir('../projects/'):
+    folder_path = os.path.join('../projects/', item)
+    
+    # Check if the item is a directory (not a file)
+    if os.path.isdir(folder_path):
         # Check if the directory already exists in the database
-        cursor.execute(check_cmd, (dir,))
+        cursor.execute(check_cmd, (item,))
         existing_project = cursor.fetchone()
 
         # If no result was found, insert the directory
         if not existing_project:
-            user_data = (dir, folder_path)
+            user_data = (item, folder_path)
             cursor.execute(insert_cmd, user_data)
 
             # Create an empty output.txt for the new dir
-            file_path = os.path.join(folder_path, 'output.txt')
-            open(file_path, 'w').close()
+            output_file_path = os.path.join(folder_path, 'output.txt')
+            open(output_file_path, 'w').close()
+
 
 # Save changes to database and close connection
 conn.commit()
