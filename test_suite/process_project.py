ef61b7c754d12636f028e4e473e39a2eb424e0c4
EMK6025 <emk6025@psu.edu>
2024-12-02 20:44:48 -0500

    rename process_folder.py to porocess_project.py and modify for sql database

--- test_suite/process_project.py
+++ test_suite/process_project.py
@@ -1,8 +1,12 @@
 import os
 import subprocess
 import sys
+import sqlite3
 
-def process_folder(proj_path, metric_name):
+
+def process_folder(proj_name, metric_name):
+    #path to project
+    proj_path = os.path.join('../projects/', proj_name)
     # Check if directory exists
     if not os.path.isdir(proj_path):
         print(f"Directory not found: {proj_path}")
@@ -17,15 +21,31 @@ def process_folder(proj_path, metric_name):
                     capture_output=True,
                     text=True
                 )
+    timestamp_cmd = "SELECT CURRENT_TIMESTAMP"
+    update_cmd = """
+        UPDATE projects
+        SET last_timestamp = ?
+        WHERE project_name = {proj_name};
+    """
+    cursor.execute(timestamp_cmd)
+    timestamp = cursor.fetchone()
+    cursor.execute(update_cmd, timestamp)
 
 if __name__ == "__main__":
+    # Relative file path to the database
+    db_path = '../database.db'
+
+    # Connect/create database
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
     # Check arguments
     if len(sys.argv) != 3:
         print("Usage: python process_folder.py <dir> <metric_name>")
         sys.exit(1)
 
     # Get values from command line arguments
-    proj_path = os.path.join('../projects/', sys.argv[1])
+    proj_name = sys.argv[1]
     metric_name = sys.argv[2]
 
-    process_folder(proj_path, metric_name)
+    process_folder(proj_name, metric_name)
