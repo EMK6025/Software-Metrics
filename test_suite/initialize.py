17bb1eb43d3774e17645f18fe30b1431f8675b0e
EMK6025 <emk6025@psu.edu>
2024-12-02 20:18:27 -0500

    integrated SQL tables instead of using text files

--- test_suite/initialize.py
+++ test_suite/initialize.py
@@ -8,17 +8,17 @@ conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
 # SQL statement to create a table
-create_projects_table_query = """
-    CREATE TABLE IF NOT EXISTS projects (
+create_projects_table_cmd = """
+    CREATE TABLE IF NOT EXISTS project (
     project_id INTEGER PRIMARY KEY AUTOINCREMENT,
     project_name TEXT NOT NULL,
-    path_to_folder TEXT NOT NULL,
-    user_id TEXT
-);
+    last_timestamp TEXT,
+    number_of_entries INTEGER
+)
 """
 
 # Execute the query
-cursor.execute(create_projects_table_query)
+cursor.execute(create_projects_table_cmd)
 
 # Save changes to database and close connection
 conn.commit()  
--- test_suite/update.py
+++ test_suite/update.py
@@ -8,27 +8,44 @@ db_path = '../database.db'
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
-insert_cmd = "INSERT INTO projects (project_name, path_to_folder) VALUES (?, ?)"
+insert_cmd = "INSERT INTO projects (project_name, last_timestamp, number_of_entries) VALUES (?, ?, ?)"
+
 check_cmd = "SELECT 1 FROM projects WHERE project_name = ? LIMIT 1"
 
-for item in os.listdir('../projects/'):
-    folder_path = os.path.join('../projects/', item)
+timestamp_cmd = "SELECT CURRENT_TIMESTAMP"
+
+for proj_name in os.listdir('../projects/'):
+    folder_path = os.path.join('../projects/', proj_name)
     
     # Check if the item is a directory (not a file)
     if os.path.isdir(folder_path):
         # Check if the directory already exists in the database
-        cursor.execute(check_cmd, (item,))
+        cursor.execute(check_cmd, (proj_name,))
         existing_project = cursor.fetchone()
 
-        # If no result was found, insert the directory
+        # If no result was found, insert the directory and create table
         if not existing_project:
-            user_data = (item, folder_path)
+            cursor.execute(timestamp_cmd)
+            timestamp = cursor.fetchone()
+            user_data = (proj_name, timestamp, 0)
             cursor.execute(insert_cmd, user_data)
 
-            # Create an empty output.txt for the new dir
-            output_file_path = os.path.join(folder_path, 'output.txt')
-            open(output_file_path, 'w').close()
+            create_projects_table_cmd = """
+                CREATE TABLE IF NOT EXISTS {dir} (
+                metric TEXT NOT NULL,
+                timestamp TEXT NOT NULL
+                value TEXT NOT NULL,
+            )
+            """
+            cursor.execute(create_projects_table_cmd)
 
+            create_projects_table_cmd = """
+                CREATE TABLE IF NOT EXISTS {dir}_cur (
+                metric TEXT NOT NULL,
+                file TEXT NOT NULL,
+                value TEXT NOT NULL
+            )
+            """ 
 # Save changes to database and close connection
 conn.commit()
 conn.close()
