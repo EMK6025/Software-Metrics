0a8cd2550bb2a6398c0924fc7cb02455a34cd4bb
EMK6025 <emk6025@psu.edu>
2025-01-05 21:40:02 -0800

    modify process_all.py to match new project structure

--- backend/test_suite/process_all.py
+++ backend/test_suite/process_all.py
@@ -3,9 +3,9 @@ import subprocess
 
 def process_all(proj_path, metric_name):
     # Iterate over all directories in the projects folder
-    for dir in os.listdir('../projects/'):
-        dir_path = os.path.join('../projects/', dir)
-        dir_path = dir_path.replace("\\", "/")
+    for dir in os.listdir('../../projects/'):
+        dir_path = os.path.join('../../projects/', dir)
+        dir_path = os.path.normpath(dir_path)
         # Iterate over all metrics in the metrics folder
         for _, _, metric in os.walk('../metrics/'):
             # Run the process_folder.py script as a subprocess for each metric
