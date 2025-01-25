7c2f3dfd89b569200ffaf5e3b72b3827381f459e
EMK6025 <ethankuocodes@gmail.com>
2025-01-24 19:26:23 -0500

    add update_required function to Github_api.py

--- backend/API/Github_api.py
+++ backend/API/Github_api.py
@@ -1,22 +1,40 @@
 from github import Github, Auth
 import os
+from datetime import datetime, timezone
 
-# Grabbing file path to keys.txt
-script_dir = os.path.dirname(os.path.abspath(__file__))
-keys_path = os.path.join(script_dir, "keys.txt")
+def update_required(author, repo_name, cut_off_date):
+    # Grabbing file path to keys.txt
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    keys_path = os.path.join(script_dir, "keys.txt")
 
-# Reading private access token
-with open(keys_path, "r") as file:
-            key = file.readline().strip()
+    # Reading private access token
+    with open(keys_path, "r") as file:
+        key = file.readline().strip()
 
-auth = Auth.Token(key)
+    # Public Web Github
+    auth = Auth.Token(key)
+    g = Github(auth=auth)
 
-# Public Web Github
-g = Github(auth=auth)
+    # Get repository
+    repo = g.get_repo(f"{author}/{repo_name}")
 
-# Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    print(repo.name)
+    cut_off_date = cut_off_date.replace(tzinfo=timezone.utc)
+    last_commit_date = repo.get_commits()[0].commit.author.date.replace(tzinfo=timezone.utc)
 
-# To close connections after use
-g.close()
+    if cut_off_date > last_commit_date: # no new commits
+        return False 
+    
+    # Iterate through commits
+    for commit in repo.get_commits():
+        print(commit.commit.author.date)
+        print(commit.commit.message)
+        print("Modified Files: ")
+        for file in commit.files:
+            print(file.filename)
+        print()
+
+    # Close connection
+    g.close()
+
+if __name__ == "__main__":
+    update_required("EMK6025", "Software-Metrics", datetime.now())
