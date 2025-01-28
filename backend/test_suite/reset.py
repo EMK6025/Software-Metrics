04942cb6d2e62d72d49b1083787c78668db31ace
EMK6025 <ethankuocodes@gmail.com>
2025-01-27 20:29:05 -0500

    add files table

--- backend/test_suite/reset.py
+++ backend/test_suite/reset.py
@@ -1,27 +1,43 @@
 import sqlite3
 
-# Relative file path to the database
-db_path = '../database.db'
+if __name__ == "__main__":
+    # Relative file path to the database
+    db_path = '../database.db'
 
-# Connect to the SQLite database
-conn = sqlite3.connect(db_path)
-cursor = conn.cursor()
+    # Connect to the SQLite database
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
 
-# SQL statement to create a table for projects
-create_projects_table_cmd = """
-    CREATE TABLE IF NOT EXISTS projects
-      (
-    project_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    project_name TEXT NOT NULL,
-    last_timestamp TEXT,
-    number_of_entries INTEGER
-)
-"""
+    # SQL statement to create a table for projects
+    create_projects_table_cmd = """
+      CREATE TABLE IF NOT EXISTS projects
+        (
+        project_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        author TEXT NOT NULL,
+        project TEXT NOT NULL,
+        last_update TEXT,
+        number_of_entries INTEGER
+        )
+      """
+    create_files_table_cmd = """
+      CREATE TABLE IF NOT EXISTS files
+        (
+        author TEXT NOT NULL,
+        project TEXT NOT NULL,
+        file_name TEXT NOT NULL,
+        last_update TEXT,
+        number_of_entries INTEGER
+        )
+      """
 
-# Execute the query to create the projects table
-cursor.execute("DROP TABLE projects")
-cursor.execute(create_projects_table_cmd)
+    # Execute the query to create the projects table
+    cursor.execute("DROP TABLE projects")
+    cursor.execute(create_projects_table_cmd)
+    cursor.execute("DROP TABLE files")
+    cursor.execute(create_files_table_cmd)
 
-# Save changes to the database and close the connection
-conn.commit()
-conn.close()
+    # Save changes to the database and close the connection
+    conn.commit()
+    conn.close()
+
+
