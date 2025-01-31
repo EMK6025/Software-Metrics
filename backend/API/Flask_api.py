cb8461fb48a6bb7031de6598d39dc4cfb3687268
EMK6025 <ethankuocodes@gmail.com>
2025-01-30 21:32:27 -0500

    update Flask_api.py

--- backend/API/Flask_api.py
+++ backend/API/Flask_api.py
@@ -1,28 +1,22 @@
-from flask import Flask, request, jsonify
-import sqlite3
+from flask import Flask, jsonify
 from flask_cors import CORS
+import sqlite3
 
-app = Flask(__name__)
-CORS(app)  # Allow cross-origin requests
-
-def get_db_connection():
+def get_sql_connection() -> sqlite3.Connection:
     conn = sqlite3.connect('./database.db')
     conn.row_factory = sqlite3.Row
     return conn
+    
+app = Flask(__name__)
+CORS(app)  # Allow cross-origin requests
 
 @app.route('/api/projects', methods=['GET'])
 
 def get_projects():
-    conn = get_db_connection()
+    conn = get_sql_connection()
     projects = conn.execute('SELECT * FROM projects').fetchall()
     conn.close()
     return jsonify([dict(project) for project in projects])
 
-def get_project_entries(proj):
-    conn = get_db_connection()
-    entries = conn.execute(f'SELECT * FROM {proj}').fetchall()
-    conn.close()
-    return jsonify([dict(entry) for entry in entries])
-
-if __name__ == '__main__':
+def main():
     app.run(debug=True)
