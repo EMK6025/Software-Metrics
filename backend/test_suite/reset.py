668c8101e1b82672fae20a26224bfb1d889aeed8
EMK6025 <ethankuocodes@gmail.com>
2025-02-02 14:22:41 -0500

    remove reserved keywords from table creation reset.py

--- backend/test_suite/reset.py
+++ backend/test_suite/reset.py
@@ -1,7 +1,10 @@
 import sqlite3
 
 if __name__ == "__main__":
-    # Relative file path to the database
+    
+
+
+# Relative file path to the database
     db_path = '../database.db'
 
     # Connect to the SQLite database
@@ -10,42 +13,43 @@ if __name__ == "__main__":
 
     # SQL statement to create a table for projects
     create_projects_table_cmd = """
-      CREATE TABLE IF NOT EXISTS projects
-        (
-        project_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        author TEXT NOT NULL,
-        project TEXT NOT NULL,
-        last_update DATETIME,
-        number_of_entries INTEGER
-        )
-      """
+        CREATE TABLE IF NOT EXISTS projects_db (
+            project_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            author TEXT NOT NULL,
+            project TEXT NOT NULL,
+            last_update DATETIME,
+            number_of_entries INTEGER
+        );
+    """
+    
     create_files_table_cmd = """
-      CREATE TABLE IF NOT EXISTS files (
-        project_id INTEGER NOT NULL,
-        FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE,
-        file TEXT NOT NULL,
-        metric TEXT NOT NULL,
-        value INTEGER NOT NULL,
-        date DATETIME DEFAULT CURRENT_TIMESTAMP,
-        UNIQUE(project_id, file, metric, date)
-    )
-      """
+        CREATE TABLE IF NOT EXISTS files_db (
+            project_id INTEGER NOT NULL,
+            file_name TEXT NOT NULL,
+            metric_name TEXT NOT NULL,
+            value INTEGER NOT NULL,
+            date DATETIME DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (project_id) REFERENCES projects_db(project_id) ON DELETE CASCADE,
+            UNIQUE(project_id, file_name, metric_name, date)
+        );
+    """
+    
     create_totals_table_cmd = """
-      CREATE TABLE IF NOT EXISTS totals
-        (
-        project_id INTEGER NOT NULL,
-        FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE,
-        metric TEXT NOT NULL,
-        value INTEGER NOT NULL,
-        date TEXT NOT NULL
-        )
-      """
+        CREATE TABLE IF NOT EXISTS totals_db (
+            project_id INTEGER NOT NULL,
+            metric_name TEXT NOT NULL,
+            value INTEGER NOT NULL,
+            date TEXT NOT NULL,
+            FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE, 
+            UNIQUE(project_id, metric_name, date)
+        );
+    """
     # Execute the query to create the projects table
-    cursor.execute("DROP TABLE IF EXISTS projects;")
+    cursor.execute("DROP TABLE IF EXISTS projects_db")
     cursor.execute(create_projects_table_cmd)
-    cursor.execute("DROP TABLE IF EXISTS files")
+    cursor.execute("DROP TABLE IF EXISTS files_db")
     cursor.execute(create_files_table_cmd)
-    cursor.execute("DROP TABLE IF EXISTS totals")
+    cursor.execute("DROP TABLE IF EXISTS totals_db")
     cursor.execute(create_totals_table_cmd)
 
     # Save changes to the database and close the connection
